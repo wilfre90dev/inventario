632a9c52a3fb4d6cf33878a8e47dbc613387e4b0
Wilfredo Palma <wilfre90dev@gmail.com>
2022-06-04 23:37:47 -0400

    Added TableLayout to better adjust items

--- app/src/main/java/cu/iviera/aftcheck/AdaptadorAFTs.java
+++ app/src/main/java/cu/iviera/aftcheck/AdaptadorAFTs.java
@@ -28,13 +28,17 @@ public class AdaptadorAFTs extends RecyclerView.Adapter<AdaptadorAFTs.ViewHolder
 
     @Override
     public void onBindViewHolder(@NonNull @org.jetbrains.annotations.NotNull AdaptadorAFTs.ViewHolderAFTs viewHolderAFTs, int i) {
-        String linea1= "<u><b>No.Inv:</b></u> "+listaAFTs.get(i).getInventario()+"\t \t \t \t <u><b>C. Costo:</b></u> "+ listaAFTs.get(i).getCentroCosto();
-        String linea2= "<u><b>Inmov:</b></u> "+listaAFTs.get(i).getInmovilizado()+"\t \t \t \t <u><b>Area:</b></u> "+listaAFTs.get(i).getArea();
-        String linea3= "<u><b>Descrip:</b></u> "+listaAFTs.get(i).getDescripcion();
+        String linea1= "<u><b>No.Inv:</b></u> "+listaAFTs.get(i).getInventario();
+        String linea2= "<u><b>Area:</b></u> "+listaAFTs.get(i).getArea();
+        String linea3= "<u><b>Inmov:</b></u> "+listaAFTs.get(i).getInmovilizado();
+        String linea4= "<u><b>C. Costo:</b></u> "+ listaAFTs.get(i).getCentroCosto();
+        String linea5= "<u><b>Descrip:</b></u> "+listaAFTs.get(i).getDescripcion();
 
         viewHolderAFTs.linea1.setText(Html.fromHtml(linea1));
         viewHolderAFTs.linea2.setText(Html.fromHtml(linea2));
         viewHolderAFTs.linea3.setText(Html.fromHtml(linea3));
+        viewHolderAFTs.linea4.setText(Html.fromHtml(linea4));
+        viewHolderAFTs.linea5.setText(Html.fromHtml(linea5));
         if (listaAFTs.get(i).isChecked) {
             viewHolderAFTs.imgCheck.setImageResource(R.drawable.checked);
         } else {
@@ -48,7 +52,7 @@ public class AdaptadorAFTs extends RecyclerView.Adapter<AdaptadorAFTs.ViewHolder
     }
 
     public class ViewHolderAFTs extends RecyclerView.ViewHolder {
-        TextView linea1, linea2, linea3;
+        TextView linea1, linea2, linea3, linea4, linea5;
         ImageView imgCheck;
 
         public ViewHolderAFTs(@NonNull @org.jetbrains.annotations.NotNull View itemView) {
@@ -58,6 +62,8 @@ public class AdaptadorAFTs extends RecyclerView.Adapter<AdaptadorAFTs.ViewHolder
             linea1= (TextView) itemView.findViewById(R.id.tvLinea1);
             linea2= (TextView) itemView.findViewById(R.id.tvLinea2);
             linea3= (TextView) itemView.findViewById(R.id.tvLinea3);
+            linea4= (TextView) itemView.findViewById(R.id.tvLinea4);
+            linea5= (TextView) itemView.findViewById(R.id.tvLinea5);
             imgCheck= (ImageView) itemView.findViewById(R.id.imgCheck);
         }
 
